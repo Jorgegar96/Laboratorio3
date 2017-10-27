59b4d7d5bae1d0d5d377e8b48a2b243f76f399ff
jorgegar96 <jorgegar896@gmail.com>
2017-10-27 22:09:00 +0200

    Clase locales comida creada

---
+++ EduardoGuevara_JorgeGarciaLab#3/src/eduardoguevara_jorgegarcialab/pkg3/LocalesComida.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eduardoguevara_jorgegarcialab.pkg3;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author JorgeLuis
+ */
+public class LocalesComida extends Locales{
+    private int cantidadEmpleados;
+    private ArrayList<Comida> productos = new ArrayList();
+    private Comida productoDia;
+    
+    
+}
